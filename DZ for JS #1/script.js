9a33e2450e2a0319b0c8679dc3216bce863c2e51
Minor353 <nikitenkogv.main@gmail.com>
2018-01-31 22:54:31 +0200

    Домашка №1

---
+++ DZ for JS #1/script.js
@@ -0,0 +1,109 @@
+//////////////Типы данных и переменные///////////////
+
+//var name = 'Gena';
+//
+//console.log(name);
+//
+//name = 'Olga';
+//
+//console.log(name);
+
+
+////////////////////Условный оператор if////////////////////////
+
+//var money = 9999,
+//    need = 5000;
+//
+//if (money > need) {
+//    console.log('Life is good!');
+//    } else {
+//        console.log('Life is s#*t!');
+//    }
+
+
+///////////////////////Циклический оператор for://////////////////////
+
+//for (var i = 0; i < 10; i++) {
+//    console.log(i);
+//}
+
+
+/////////////////////////Функции:////////////////////
+//
+//var sum = function(p1,p2,p3){
+//    var sumNum = p1 + p2 + p3;
+//    
+//    return sumNum;
+//}
+//
+//
+//var sumNum = sum(10,20,30);;
+//
+//console.log(sumNum);
+//
+//var sumNum = sum(15,25,35);;
+//
+//console.log(sumNum);
+//
+//var sumNum = sum(20,30,40);;
+//
+//console.log(sumNum);
+
+////////////////////Массивы и объекты:////////////////////////////
+
+//
+//var name = ['привет', 'loftschool'];
+//
+//name.push('я изучаю', 'javascript');
+//
+//console.log(name.length);
+//
+//for (var i = 0; i < name.length; i++) {
+//		console.log(name[i]);
+//}
+
+
+//////////////////////////////////////////////////
+
+//var array = [10, 555, 112, 656, 25, 44, 25, 66, 155, 87];
+//
+//for (var i = 0; i < array.length; i++) {
+//    if (array[i] > 100) {
+//        console.log(array[i]);
+//        }
+//}
+
+
+//////////////////////////////////////////////////
+
+
+//var passport = {
+//    name: 'Gena',
+//    lastName: 'Nikitenko',
+//    age: 26
+//};
+//
+//console.log(passport.name);
+//console.log(passport.lastName);
+//console.log(passport.age);
+//
+//passport.serial = 'AE';
+//
+//console.log(passport.serial);
+
+////////////////////////////////////////////////////
+
+//var passport = {
+//    name: 'Gena',
+//    lastName: 'Nikitenko',
+//    age: 26
+//};
+//
+//
+//var hello = function(human) {
+//    return 'Привет, меня зовут ' + passport.name + ' ' + passport.lastName + ' и мне ' + passport.age + ' лет!';
+//}
+//
+//var result = hello(passport);
+//
+//console.log(result);
